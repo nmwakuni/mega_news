cf92fce3d44e37a7e09504437e17c7ae780d6ddc
nico <nico@zuku.www.zuku.com>
2023-11-01 00:26:35 +0300

    cat

--- components/categoryList/CategoryList.jsx
+++ components/categoryList/CategoryList.jsx
@@ -23,7 +23,7 @@ const CategoryList = async () => {
       <div className={styles.categories}>
         {data?.map((item) => (
           <Link
-            href={`/blog/`}
+            href={`/blog/cat=style`}
             className={`${styles.category} ${styles[item.slug]}`}
             key={item._id}
           >
